14bdb9b4000aa9520d49ed01206eaeef8c210aa2
kino lee <kinoflee@gmail.com>
2015-05-14 21:48:38 +0200

    redesign from scratch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,33 +18,97 @@
  */
 (function () {
   'use strict';
+  /*
+  oneLineType.js v0.1 by kino (http://42web.co)
+  fork from
+* FlowType.JS v1.1 by Simple Focus (http://simplefocus.com/)
+*
+* oneLineType.JS 
+* is licensed under the MIT License. Read a copy of the
+* license in the LICENSE.txt file or at
+* http://choosealicense.com/licenses/mit
+*
+* Thanks to Giovanni Difeterici (http://www.gdifeterici.com/)
+*/
 
-  var querySelector = document.querySelector.bind(document);
+(function($) {
+   $.fn.onelinetype = function(options) {
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
+// Establish default settings/variables
+// ====================================
+      var settings = $.extend({
+         maximum   : 9999,
+         minimum   : 1,
+         maxFont   : 9999,
+         minFont   : 1,
+         fontSpacing : 1
+      }, options),
 
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
+// Do the magic math
+// =================
+      changes = function(el) {
+         var $el = $(el),
+            elw = $el.width(),
+            textCount = $.trim($el.text()).length,
+            spaceCount = (textCount-1) * settings.fontSpacing,
+            width = elw > settings.maximum ? settings.maximum : elw < settings.minimum ? settings.minimum : elw,
+            fontBase = width / (textCount+spaceCount),
+            fontSize = fontBase > settings.maxFont ? settings.maxFont : fontBase < settings.minFont ? settings.minFont : fontBase;
+         $el.css({
+          'font-size': fontSize + 'px',
+          'letter-spacing': settings.fontSpacing + 'em',
+          'text-align':'center',
+          'margin-left': settings.fontSpacing/2 + 'em'
+        });
+      };
 
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
+// Make the magic visible
+// ======================
+      return this.each(function() {
+      // Context for resize callback
+         var that = this;
+      // Make changes upon resize
+         $(window).resize(function(){changes(that);});
+      // Set changes on load
+         changes(this);
+      });
+   };
+}(jQuery));
 
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
+$(".logo").onelinetype({
+  fontSpacing : 2.8
+});
+
+$(".info").onelinetype({
+  fontSpacing : 1.4
+});
+
+  // var querySelector = document.querySelector.bind(document);
+
+  // var navdrawerContainer = querySelector('.navdrawer-container');
+  // var body = document.body;
+  // var appbarElement = querySelector('.app-bar');
+  // var menuBtn = querySelector('.menu');
+  // var main = querySelector('main');
+
+  // function closeMenu() {
+  //   body.classList.remove('open');
+  //   appbarElement.classList.remove('open');
+  //   navdrawerContainer.classList.remove('open');
+  // }
+
+  // function toggleMenu() {
+  //   body.classList.toggle('open');
+  //   appbarElement.classList.toggle('open');
+  //   navdrawerContainer.classList.toggle('open');
+  //   navdrawerContainer.classList.add('opened');
+  // }
+
+  // main.addEventListener('click', closeMenu);
+  // menuBtn.addEventListener('click', toggleMenu);
+  // navdrawerContainer.addEventListener('click', function (event) {
+  //   if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+  //     closeMenu();
+  //   }
+  // });
 })();
