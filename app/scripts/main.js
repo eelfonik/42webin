8ed7c2dbaa41dde9dc550d46b554e2f1ed5fead5
kino lee <kinoflee@gmail.com>
2016-01-07 18:29:17 +0100

    v1.3.2
    before change structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,10 +22,47 @@
           .to(".info", 0.1, {y:"15%", rotationX:-90, transformOrigin:"50% bottom 0"},0);
           // .to(".app-bar",0.0001, {x:"100%"})
 
+
+    function centerEl(el) {
+        var elOffset = el.offset().top;
+        var elHeight = el.height();
+        var windowHeight = $(window).height();
+        var offset;
+
+        if (elHeight < windowHeight) {
+          offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+        }
+        else {
+          offset = elOffset;
+        }
+
+        $('html, body').animate({
+              scrollTop: offset
+          }, 500);
+    }
+
+    function isScrolledIntoView(elem){
+          var centerY = Math.max(0,(($(window).height()-$(elem).outerHeight()) / 2) + $(window).scrollTop());
+
+          var elementTop = $(elem).offset().top;
+          var elementBottom = elementTop + $(elem).height();
+
+          return elementTop <= centerY && elementBottom >= centerY;
+      }
+
+
   
       var position = $(window).scrollTop();
 
       $(window).scroll(function() {
+        $(".projet").each(function() {
+            if(isScrolledIntoView($(this))) {
+              if(!$(this).hasClass("clicked")) {
+                  $(this).addClass("clicked");
+              }
+              $(this).parent(".works").siblings().find(".projet").removeClass("clicked");
+            }
+        });
         var scroll = $(window).scrollTop();
         if (scroll > position) {
           // scrolling downwards
@@ -88,24 +125,12 @@
       e.preventDefault();
       var $this= $(this);
       var $container = $this.parent(".works");
-      $container.addClass("clicked");
-      $container.siblings().removeClass("clicked");
       var $projet = $this.siblings(".projet");
-      var elOffset = $container.offset().top;
-      var elHeight = $container.height();
-      var windowHeight = $(window).height();
-      var offset;
-
-      if (elHeight < windowHeight) {
-        offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+      if(!$projet.hasClass("clicked")) {
+          $projet.addClass("clicked");
       }
-      else {
-        offset = elOffset;
-      }
-
-      $('html, body').animate({
-            scrollTop: offset
-        }, 500);
+      $container.siblings().find(".projet").removeClass("clicked");
+      centerEl($projet);
   });
     
   });
